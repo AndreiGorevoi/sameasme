6779c21a66906a5648de7d21728e776baa18afd8
Andrei Gorevoi <18.andreigorevoi@gmail.com>
2020-06-22 17:29:06 +0300

    Initial commit

---
+++ src/main/java/com/tms/sameasme/SameasmeApplication.java
@@ -0,0 +1,13 @@
+package com.tms.sameasme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SameasmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SameasmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tms/sameasme/SameasmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.sameasme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SameasmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
